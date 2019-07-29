0959d69fb0338dd8f4994d90ffb311e356dd7d8b
Adrian Siñiga <ajaxwd@gmail.com>
2019-07-28 23:47:38 -0400

    model user

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,17 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
+    POSTGRES = {
+    'user': 'adrian',
+    'pw': 'miniblog',
+    'db': 'miblog',
+    'host': 'localhost',
+    'port': '5432',
+    }
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+    %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
     app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:testing@localhost:5432/miniblog'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     login_manager.init_app(app)
---
+++ app/model/__init__.py
@@ -0,0 +1,2 @@
+from app import db
+from .userModel import User
